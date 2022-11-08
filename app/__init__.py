8eeb59c10427fa7ef31ad398cda859a2d26aac1e
Yat Long Chan <yatlongc2005@gmail.com>
2022-11-08 16:11:46 -0500

    rudimentary login

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, session, request, redirect
 
 app = Flask(__name__)
 
+# testing purposes
+user = 'frodo' 
+passwd = 'baggins'
+
 app.secret_key = "HI" # dummy key
 
 @app.route('/', methods=['GET'])
@@ -12,6 +16,21 @@ def login():
 
 @app.route('/auth', methods=['POST'])
 def authenticate():
+  # will check if info is in db later on
+  if request.form['username'] != user and request.form['username'] != passwd: 
+    return render_template('login.html', status='Incorrect login info')
+  
+  session['username'] = request.form['username']
+  return redirect('/home')
+
+@app.route('/home', methods=['GET'])
+def home():
+  return render_template('homepage.html')
+
+@app.route('/logout')
+def logout():
+  session.pop('username')
+  return redirect('/')
   
 if __name__ == '__main__':
   app.debug = True
